d3accbbe10b6b38c3c93e493a9021a32059b320d
ganeshalapati <alapatiganesh31@gmail.com>
2021-08-09 09:22:24 +0530

    Added Hotel to Reservation System

---
+++ Hotel.java
@@ -0,0 +1,28 @@
+
+public class Hotel {
+	private String name;
+	private int rate;
+	public Hotel(String name, int rate) {
+		super();
+		this.name = name;
+		this.rate = rate;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRate() {
+		return rate;
+	}
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+	public String toString() {
+		return "Hotel [name=" + name + ", rate=" + rate + "]";
+	}
+	
+
+
+}
--- Hotel_Reservation_SYstem.java
+++ Hotel_Reservation_SYstem.java
@@ -1,9 +1,37 @@
+import java.util.ArrayList;
 
 public class Hotel_Reservation_SYstem {
+	//ArrayList of class Hotel 
+		private static ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
+
+		
+		public ArrayList<Hotel> getHotelList() {
+			return hotelList;
+		}
+
+		/*Add hotel from Hotel.java
+		 * creating object oh hotel class
+		 * add object to ArrayList  to store Hotel data
+		 * */
+		public static void addHotel(String name, int price) {
+			Hotel hotel = new Hotel(name, price);
+			hotelList.add(hotel);
+		}
+
+		public static int countNoOfHotels() {
+			return hotelList.size();//getting no of hotels
+		}
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Hotel Reservation System");
+		Hotel lakeWood = new Hotel("Lakewood", 110);
+		Hotel bridgeWood = new Hotel("Bridgewood", 110);
+		hotelList.add(lakeWood);
+		hotelList.add(bridgeWood);
+		System.out.println(hotelList);
+
+
 	}
 
 }
